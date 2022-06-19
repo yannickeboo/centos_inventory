7a5801cfb1fa7cd0d456df0114fb1a37dc66b85e
yannick <yannickeboo@gmail.com>
2022-06-19 21:36:16 +0000

    adding docker installation file

---
+++ docker_installation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+sudo yum install -y yum-utils
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sudo systemctl start docker
+sudo systemctl enable docker
